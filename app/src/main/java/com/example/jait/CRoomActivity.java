fc33355916109a99b50877b5dcd36e4524b74616
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2020-11-25 07:03:26 +0900

    array postId

---
+++ app/src/main/java/com/example/jait/CRoomActivity.java
@@ -0,0 +1,13 @@
+package com.example.jait;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_room);
+    }
+}
